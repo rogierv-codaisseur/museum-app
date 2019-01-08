277c29afcc22e840b1f1bc3cad216727b54b810b
Rogier Verkaik <rogierv@gmail.com>
2019-01-08 22:27:22 +0100

    Display elements and reset form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,15 +1,28 @@
 function submitComment() {
+  // gather data
   const inputField = document.getElementById('name');
   const name = inputField.value;
   const textArea = document.getElementById('msg');
   const msg = textArea.value;
+
+  // create the elements you need
   const comment = document.createElement('section');
   const h3 = document.createElement('h3');
   const p = document.createElement('p');
+
+  // adjust the elements we created
   h3.innerHTML = `${name} said:`;
   p.innerHTML = msg;
   comment.classList.add('comment');
   comment.appendChild(h3);
   comment.appendChild(p);
-  console.log(comment);
+
+  // display the elements on the page
+  const commentSection = document.getElementById('comments');
+  commentSection.appendChild(comment);
+
+  // reset form values
+  inputField.value = null;
+  textArea.value = null;
+
 }
